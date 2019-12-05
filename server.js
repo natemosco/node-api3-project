ea75b7f1f57b2c55ad20f59147535d159f152e9c
Nathaniel Mosco <natemosco@gmail.com>
2019-12-04 23:14:33 -0500

    index.js and server.js boilerplate complete for now. get to / working

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet"); //<<<<security package import
 
+const postRouter = require("./posts/postRouter");
+const usersRouter = require("./users/userRouter");
+
+//define custom middleware
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl}`);
+  next();
+}
+
+const middleware = [helmet(), express.json(), logger];
+
+//declare your server
 const server = express();
+//assign global middleware to endpoints
+server.use("api/user", middleware, usersRouter);
+server.use("api/post", middleware, postRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
-
-//custom middleware
-
-function logger(req, res, next) {}
 
 module.exports = server;
