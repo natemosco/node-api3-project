d03e4ebf2d5cf1a1100581f42a7e1e0615a638db
Nathaniel Mosco <natemosco@gmail.com>
2019-12-06 09:06:23 -0500

    added process.env.PORT added start script to pkg.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 const server = require("./server");
-const port = 3459;
+const port = process.env.PORT || 3459;
 server.listen(port, () => {
   console.log(
     "\n* Welcome back Sir, your server is runing on http://localhost:3459 *\n"
